5397881b8d958fb0307e16df8f99f1bd4e04ad61
Kentaro Umetsu <umetsu.kentaro@adways.net>
2020-05-18 12:22:52 +0900

    :recycle: findByを使ってテストに柔軟性をもたせた

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,34 +3,37 @@ import user from "@testing-library/user-event"
 import React from "react"
 import { App } from "./App"
 
-test("タスクの追加と一覧表示", () => {
-  const { getByText, getAllByText, getByLabelText, queryByText } = render(
+test("タスクの追加と一覧表示", async () => {
+  const { findByText, findAllByText, findByLabelText, queryByText } = render(
     <App />
   )
-  expect(getByText(/empty/i)).toBeInTheDocument() // Emptyが表示されていること
+  expect(await findByText(/empty/i)).toBeInTheDocument()
 
-  const taskInput = getByLabelText(/todo/i)
-  const addButton = getByText(/add/i)
+  const taskInput = await findByLabelText(/todo/i)
+  const addButton = await findByText(/add/i)
 
   // 空文字ならタスクは追加されない
-  user.type(taskInput, "")
+  await user.type(taskInput, "")
   user.click(addButton)
-  expect(getByText(/empty/i)).toBeInTheDocument()
+  expect(await findByText(/empty/i)).toBeInTheDocument()
 
   // スペースでも同様に追加されない
-  user.type(taskInput, " ")
+  await user.type(taskInput, " ")
   user.click(addButton)
-  expect(getByText(/empty/i)).toBeInTheDocument()
+  expect(await findByText(/empty/i)).toBeInTheDocument()
 
   // 1件表示
-  user.type(taskInput, "task1")
+  await user.type(taskInput, "task1")
   user.click(addButton)
-  expect(getByText(/task1/i)).toBeInTheDocument()
+  expect(await findByText(/task1/i)).toBeInTheDocument()
   expect(taskInput).toHaveValue("")
-  expect(queryByText(/empty/i)).not.toBeInTheDocument() // Emptyが非表示になっていること
+  expect(queryByText(/empty/i)).not.toBeInTheDocument()
+
+  // 2件目追加
+  await user.type(taskInput, "task2")
+  user.click(addButton)
+  expect(await findByText(/task2/i)).toBeInTheDocument()
 
   // 複数件表示
-  user.type(taskInput, "task2")
-  user.click(addButton)
-  expect(getAllByText(/task./i)).toHaveLength(2)
+  expect(await findAllByText(/task./i)).toHaveLength(2)
 })
